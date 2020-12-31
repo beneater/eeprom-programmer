ed7e7044a975d7232e8671437a73ee53daac4d65
Subhankar Karmakar <subhu990@gmail.com>
2021-01-01 01:08:50 +0530

    Update microcode-eeprom-with-flags__FORMATTED.ino

--- ROM DUMP WITHOUT HAVING A EEPROM/microcode-eeprom-with-flags__FORMATTED/microcode-eeprom-with-flags__FORMATTED.ino
+++ ROM DUMP WITHOUT HAVING A EEPROM/microcode-eeprom-with-flags__FORMATTED/microcode-eeprom-with-flags__FORMATTED.ino
@@ -27,22 +27,22 @@
 #define JZ  0b1000
 
 uint16_t UCODE_TEMPLATE[16][8] = {
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 0000 - NOP
-  { MI | CO,  RO | II | CE,  IO | MI,  RO | AI,  0,           0, 0, 0 }, // 0001 - LDA
-  { MI | CO,  RO | II | CE,  IO | MI,  RO | BI,  EO | AI | FI,    0, 0, 0 }, // 0010 - ADD
-  { MI | CO,  RO | II | CE,  IO | MI,  RO | BI,  EO | AI | SU | FI, 0, 0, 0 }, // 0011 - SUB
-  { MI | CO,  RO | II | CE,  IO | MI,  AO | RI,  0,           0, 0, 0 }, // 0100 - STA
-  { MI | CO,  RO | II | CE,  IO | AI,  0,      0,           0, 0, 0 }, // 0101 - LDI
-  { MI | CO,  RO | II | CE,  IO | J,   0,      0,           0, 0, 0 }, // 0110 - JMP
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 0111 - JC
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 1000 - JZ
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 1001
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 1010
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 1011
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 1100
-  { MI | CO,  RO | II | CE,  0,      0,      0,           0, 0, 0 }, // 1101
-  { MI | CO,  RO | II | CE,  AO | OI,  0,      0,           0, 0, 0 }, // 1110 - OUT
-  { MI | CO,  RO | II | CE,  HLT,    0,      0,           0, 0, 0 }, // 1111 - HLT
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 0000 - NOP
+  { MI|CO,  RO|II|CE,  IO|MI,  RO|AI,  0,           0, 0, 0 },   // 0001 - LDA
+  { MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI|FI,    0, 0, 0 },   // 0010 - ADD
+  { MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI|SU|FI, 0, 0, 0 },   // 0011 - SUB
+  { MI|CO,  RO|II|CE,  IO|MI,  AO|RI,  0,           0, 0, 0 },   // 0100 - STA
+  { MI|CO,  RO|II|CE,  IO|AI,  0,      0,           0, 0, 0 },   // 0101 - LDI
+  { MI|CO,  RO|II|CE,  IO|J,   0,      0,           0, 0, 0 },   // 0110 - JMP
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 0111 - JC
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 1000 - JZ
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 1001
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 1010
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 1011
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 1100
+  { MI|CO,  RO|II|CE,  0,      0,      0,           0, 0, 0 },   // 1101
+  { MI|CO,  RO|II|CE,  AO|OI,  0,      0,           0, 0, 0 },   // 1110 - OUT
+  { MI|CO,  RO|II|CE,  HLT,    0,      0,           0, 0, 0 },   // 1111 - HLT
 };
 
 uint16_t ucode[4][16][8];
